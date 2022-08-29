049b0911d3339e1603f5b1e6cc01a8854e7173bb
Leandro Menezes <leandromenezes1@gmail.com>
2022-08-28 21:46:54 -0300

    feat: start project, create dbContext, models and migrations

---
+++ APICatalogo/Migrations/20220828234022_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemUrl) Values ('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemUrl) Values ('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome, ImagemUrl) Values ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Categorias");
+        }
+    }
+}
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,15 +1,28 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Services
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#endregion
+
+
+#region DbContext
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+                    options.UseMySql(mySqlConnection, 
+                    ServerVersion.AutoDetect(mySqlConnection)));
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
